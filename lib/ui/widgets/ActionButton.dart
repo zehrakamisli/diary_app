7bbbac301587bf4490487dd27e04fa56dce42cf6
Zehra Kamisli <kamsl.zehra97@gmail.com>
2022-12-28 00:18:25 +0300

    FAB Add

---
+++ lib/ui/widgets/ActionButton.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ActionButton extends StatelessWidget {
+  const ActionButton({
+    super.key,
+    this.onPressed,
+    required this.icon,
+    required this.backgroundColor,
+  });
+
+  final VoidCallback? onPressed;
+  final Widget icon;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Material(
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      color: backgroundColor,
+      elevation: 4.0,
+      child: IconButton(
+        onPressed: onPressed,
+        icon: icon,
+        color: theme.colorScheme.onSecondary,
+      ),
+    );
+  }
+}
